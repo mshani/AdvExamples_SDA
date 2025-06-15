64d13b656495938152ff4e9b8a371459b4c794db
Marsela Shani <m.shani.ita@be-tse.it>
2025-06-15 12:07:13 +0200

    Add CRUD for expenses

--- SmartExpences/Controllers/HomeController.cs
+++ SmartExpences/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using SmartExpenses.Models;
+using SmartExpenses.Services.Infrastucture;
 using System.Diagnostics;
 
 namespace SmartExpenses.Controllers
@@ -7,26 +8,59 @@ namespace SmartExpenses.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IExpenseService _expenseService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IExpenseService expenseService)
         {
             _logger = logger;
+            _expenseService = expenseService;
         }
 
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult Overview()
+
+        public async Task<IActionResult> Overview()
         {
+            var expenses = await _expenseService.GetAllExpensesAsync(); 
+            return View(expenses);
+        }
+
+        public async Task<IActionResult> UpsertExpense(int? id)
+        {
+            if (id != null)
+            {
+                var expense = await _expenseService.GetExpenseByIdAsync(id.Value);
+                return View(expense);
+            }
             return View();
         }
 
-        public IActionResult UpsertExpense()
+        public async Task<IActionResult> DeleteExpense(int? id)
         {
-            return View();
+            if (id != null)
+            {
+                await _expenseService.DeleteExpenseAsync(id.Value);
+            }
+            return RedirectToAction("Overview");
         }
 
+        public async Task<IActionResult> UpsertForm(Expense payload)
+        {
+            if (payload != null)
+            {
+                if (payload?.Id > 0)
+                {
+                    await _expenseService.UpdateExpenseAsync(payload);
+                }
+                else
+                {
+                    await _expenseService.CreateExpenseAsync(payload);
+                }
+            }          
+            return RedirectToAction("Overview");
+        }
         public IActionResult Privacy()
         {
             return View();
